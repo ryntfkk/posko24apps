c99257267fa62de09ffe92638dcbf1d69a44417d
posko24 apps <ryntfk@gmail.com>
2025-09-09 12:30:40 -0700

    Initial commit

--- app/src/main/java/com/example/posko24/ui/orders/ReviewScreen.kt
+++ app/src/main/java/com/example/posko24/ui/orders/ReviewScreen.kt
@@ -3,6 +3,7 @@ package com.example.posko24.ui.orders
 import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.padding
+import androidx.compose.material3.ExperimentalMaterial3Api
 import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Scaffold
 import androidx.compose.material3.Text
@@ -12,6 +13,7 @@ import androidx.compose.runtime.Composable
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 
+@OptIn(ExperimentalMaterial3Api::class)
 @Composable
 fun ReviewScreen(orderId: String) {
     Scaffold(
