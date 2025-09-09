890f8547df8b7d10785146c11ad5cd74da1c2eb6
posko24 apps <ryntfk@gmail.com>
2025-09-09 13:50:05 -0700

    Initial commit

--- app/src/main/java/com/example/posko24/ui/components/InteractiveMapView.kt
+++ app/src/main/java/com/example/posko24/ui/components/InteractiveMapView.kt
@@ -4,6 +4,7 @@ import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.height
+import androidx.compose.foundation.layout.padding
 import androidx.compose.material.icons.Icons
 import androidx.compose.material.icons.filled.LocationOn
 import androidx.compose.material3.Icon
