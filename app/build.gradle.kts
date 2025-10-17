c90eb05d21baee291be0fcbd65ac882f89c4c425
posko24 apps <ryntfk@gmail.com>
2025-10-17 02:48:31 -0700

    Initial commit

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -17,12 +17,12 @@ plugins {
 
 android {
     namespace = "com.example.posko24"
-    compileSdk = 34
+    compileSdk = 35
 
     defaultConfig {
         applicationId = "com.example.posko24"
         minSdk = 24
-        targetSdk = 34
+        targetSdk = 35
         versionCode = 1
         versionName = "1.0"
         testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
